49877cf123ec33942038af0000ccfc8ea77a743a
aguirre-ivan <aguirre.ivang@gmail.com>
2022-03-02 01:54:36 -0300

    disney_characters functions

--- disney_bot.py
+++ disney_bot.py
@@ -1,7 +1,7 @@
 import tweepy, time
 from credentials import *
 from image_managment import create_image, remove_image
-from disney_characters import get_random_character_id, get_tweet_text, get_disney_character_url_image
+from disney_characters import get_random_disney_character
 
 IMAGE_NAME = "image.png"
 WAITING_TIME = 28800 # secs = 8 hours
@@ -16,15 +16,12 @@ def twitter_api_setup():
 
 
 def create_tweet(bot):
-    id_character = get_random_character_id()
+    character_info, url_image = get_random_disney_character()
 
-    tweet = get_tweet_text(id_character)
-
-    url_image = get_disney_character_url_image(id_character)
     create_image(url_image, IMAGE_NAME)
     media = bot.media_upload(IMAGE_NAME)
 
-    bot.update_status(tweet, media_ids=[media.media_id])
+    bot.update_status(character_info, media_ids=[media.media_id])
 
     remove_image(IMAGE_NAME)
 
