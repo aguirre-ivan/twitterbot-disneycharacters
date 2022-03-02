f57ea07198f5a228520adc959c4652e878a3d752
aguirre-ivan <aguirre.ivang@gmail.com>
2022-03-02 01:53:18 -0300

    disney_characters functions

--- disney_characters.py
+++ disney_characters.py
@@ -1,15 +1,41 @@
 import requests
+from random import randint
 
-MAX_CHARACTER_ID = 1
-
-
-def get_random_character_id():
-    pass
+MAX_CHARACTER_ID = 7526
+URL_CHARACTER = 'https://api.disneyapi.dev/characters/'
 
 
-def get_tweet_text(id_character):
-    pass
+def get_character_info(character_json):
+    films = character_json['films']
+    tvShows = character_json['tvShows']
+
+    films = films if len(films) != 0 else ['None']
+    tvShows = tvShows if len(tvShows) != 0 else ['None']
+
+    text_info = f"Films: {','.join(films)}\nTV Shows: {','.join(tvShows)}"
+
+    return text_info
 
 
-def get_disney_character_url_image(id_character):
-    pass
+def get_character_url_image(character_json):
+    return character_json['imageUrl']
+
+
+def get_random_disney_character():
+    random_id = randint(0, MAX_CHARACTER_ID)
+
+    url_character = f"{URL_CHARACTER}{random_id}"
+
+    character_request = requests.get(url_character)
+
+    try:
+        character_request = character_request.json()
+    except:
+        return get_random_disney_character()
+
+    character_json = character_request.json()
+
+    character_info = get_character_info(character_json)
+    character_url_image = get_character_url_image(character_json)
+
+    return character_info, character_url_image
